331492e2786ba6cf06f094573b141d2ad6b81df8
mdeejay <mdjrussia@gmail.com>
2021-12-22 13:35:25 +0530

    vince: Update camera blobs from HM5PlusGlobal 8.5.9_8.1

--- device.mk
+++ device.mk
@@ -27,9 +27,6 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     libdng_sdk.vendor
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0-v27.so
-
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
