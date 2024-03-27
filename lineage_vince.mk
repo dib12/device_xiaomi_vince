a5ce23017ea46089e10d9f59a66ca00dd156bbd9
Dibyaranjan Pradhan <61652361+dib12@users.noreply.github.com>
2024-03-27 13:04:13 +0530

    vince: Update build fingerprint

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -26,8 +26,8 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+#   PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V11.0.2.0.OEGMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/sunfish/sunfish:13/TQ3A.230805.001.S1/10786265:user/release-keys"
